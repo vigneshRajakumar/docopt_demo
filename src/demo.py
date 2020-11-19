0e4b729bdc69eda10f87f886fb0e4798303efa45
vigneshRajakumar <vigneshr.93@gmail.com>
2020-11-19 14:00:16 -0800

    Add optional position argument t python script

--- src/demo.py
+++ src/demo.py
@@ -1,16 +1,19 @@
-# author: Tiffany Timbers
-# date: 2020-01-15
+# author: Vignesh Rajakumar
+# citation: Adapted from TIffany Timber's code in UBC MDS DSCI 522 
+# date: 2020-11-19
 
 """This script prints out docopt args.
-Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg4>]          Takes any value (this is an optional positional argument)
 """
 
 from docopt import docopt
 opt = docopt(__doc__)
 print(opt)
 print(type(opt))
+print(opt['<arg4>'])
